f0279a039a2eaf86b94ea4bbeef674e1f63976bc
prerana-xworkz <Bulbule.xworkz@outlook.com>
2025-03-11 11:24:56 +0530

    committing java file with an example of Speaker

---
+++ Speaker.java
@@ -0,0 +1,12 @@
+class Speaker{
+	
+	public static void main(String[] args)
+	{
+	 System.out.println("My name is  prerana Sadanand Bulbule, Currently pursuing engineering at JNNCE Shivamogga.");
+	 System.out.println("I am 21 years old");
+	 System.out.println("My Email address is preranabulbule17@gmail.com");
+	 System.out.println("My phone number is 9353838807");
+	 System.out.println("I am doing internship at xworkz rajajjinagar,Banglore.");
+	}
+}
+	
