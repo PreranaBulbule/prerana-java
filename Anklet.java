7b8a39c9fa4081395e520525d2088285d48107f6
prerana-xworkz <Bulbule.xworkz@outlook.com>
2025-03-16 11:25:39 +0530

    adding Anklet file

---
+++ Anklet.java
@@ -0,0 +1,13 @@
+class Anklet {
+
+    String material;
+    double length;
+    boolean hasCharms;
+
+    Anklet(String material, double length, boolean hasCharms) {
+        this.material = material;
+        this.length = length;
+        this.hasCharms = hasCharms;
+        System.out.println("String, double, boolean param of Anklet");
+    }
+}
