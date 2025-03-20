400270e5225b697f317184c5780192e4f650e203
prerana-xworkz <Bulbule.xworkz@outlook.com>
2025-03-20 22:39:49 +0530

    Adding Clip file

---
+++ javaint1/src/com/xworkz/Classes/Clip.java
@@ -0,0 +1,11 @@
+package com.xworkz.Classes;
+
+public class Clip {
+        String color;
+        public Clip(String color){
+            this.color = color;
+        }
+        void hold(){
+            System.out.println("Running hold in Clip");
+        }
+    }
---
+++ javaint1/src/com/xworkz/Classes/Pushpa.java
@@ -0,0 +1,20 @@
+package com.xworkz.Classes;
+
+public class Pushpa {
+    Clip[] clips;
+
+    public Pushpa(Clip[] clips){
+        this.clips = clips;
+    }
+    void useClip(Clip[] clips){
+        for(Clip clip : clips){
+            if(clip != null){
+                System.out.println("Clip color : "+clip.color);
+                clip.hold();
+            }
+            else{
+                System.err.println("The value of clip is null");
+            }
+        }
+    }
+}
