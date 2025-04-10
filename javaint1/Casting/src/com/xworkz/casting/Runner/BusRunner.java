6b5133c433c026cd42db78eb17059f40206363de
prerana-xworkz <Bulbule.xworkz@outlook.com>
2025-04-10 13:39:52 +0530

    adding toString file

--- javaint1/Casting/src/com/xworkz/casting/Runner/BusRunner.java
+++ javaint1/Casting/src/com/xworkz/casting/Runner/BusRunner.java
@@ -5,6 +5,9 @@ public class BusRunner {
     public static void main(String[] args) {
         Bus cityBus = new Bus("42B", "City Bus", 25.00);
         System.out.println(cityBus);
+        System.out.println("Custom hashCode (overridden): " + cityBus.hashCode());
+        System.out.println("Original identityHashCode: " + System.identityHashCode(cityBus));
     }
 }
+
 
