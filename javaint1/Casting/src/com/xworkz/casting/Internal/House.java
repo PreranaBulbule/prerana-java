a5b3ccd5b820289c3b8956d61ad2925f0bfd20fc
prerana-xworkz <Bulbule.xworkz@outlook.com>
2025-04-13 11:42:32 +0530

    adding equals file

--- javaint1/Casting/src/com/xworkz/casting/Internal/House.java
+++ javaint1/Casting/src/com/xworkz/casting/Internal/House.java
@@ -13,11 +13,37 @@ public class House {
 
     @Override
     public String toString() {
-        return "House [address=" + address + ", numberOfRooms=" + numberOfRooms + ", price=" + price + "]";
+        return "House{address=" + address + ", numberOfRooms=" + numberOfRooms + ", price=" + price + "}";
     }
+
     @Override
     public int hashCode() {
-        System.out.println("HouseHashCode (from super): " + super.hashCode());
-        return 2200;
+        System.out.println("HouseHashCode: " + super.hashCode());
+        return 1857;  // Custom hash code value for House
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setNumberOfRooms(int numberOfRooms) {
+        this.numberOfRooms = numberOfRooms;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj != null) {
+            System.out.println("ref is not null");
+            if (obj instanceof House) {
+                System.out.println("have ref. will compare..");
+                House house1 = this;
+                House house2 = (House) obj;
+                if (house1.address.equals(house2.address) && house1.numberOfRooms == house2.numberOfRooms && house1.price == house2.price) {
+                    System.out.println("both are same");
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
